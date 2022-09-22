2a66e918d61dc89ff2f18fdefccb98b78cf3fc6d
Aleksey Boyarkin <alekseyretouch@yandex.ru>
2022-09-22 20:57:02 +0300

    final

---
+++ 27 задача/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+    Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+    452 -> 11
+    82 -> 10
+    9012 -> 12
+    // int counter = Convert.ToString(MyArray).Length;
+*/
+
+Console.WriteLine("Введите сумму: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (number > 0)
+{
+    int a = number % 10;
+
+    number = number / 10;
+    sum = sum + a;
+}
+
+Console.WriteLine(sum);
---
+++ 29 задача/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int[] MyArray;
+MyArray = new int[8];
+int index = 0;
+int a = 1;
+
+for (int i = 0; i < MyArray.Length; i++)
+{
+    Console.Write($"Введите {a} число: ");
+    MyArray[i] = int.Parse(Console.ReadLine());
+    a++;
+}
+
+Console.Write("Массив = [ ");
+while (index < MyArray.Length)
+{
+    Console.Write($"{MyArray[index]}, ");
+    index++;
+}
+Console.Write("]");
+Console.WriteLine();
